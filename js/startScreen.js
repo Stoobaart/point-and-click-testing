1135f2320e272c1bf1c24fa6fce84a6538825f08
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-04-14 23:47:27 +0100

    game start screen added and theme music added

--- js/startScreen.js
+++ js/startScreen.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
+  $(".startScreen").hide();
+  setTimeout(function() {
+    $(".startScreen").fadeIn(1500);
+    $("#themeMusic")[0].play()
+  }, 1000);
   $(".startGameBtn").click(function() {
     $(".startScreen").fadeOut(1500);
+    $("#themeMusic").animate({volume: 0}, 1500);
     setTimeout(function() {
       $(".menuArea, .sceneOneScreen").fadeIn(2000);
       $("#crimeSceneMusic")[0].play();
+      $("#themeMusic").pause();
     }, 1500);
   });
 });
