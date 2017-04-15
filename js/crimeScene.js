e498d81ae16815e4ea7f7c7525eaa10338a1cead
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-04-15 20:41:50 +0100

    artwork greatly improved, speach boxes created to make text more readable, dried up conversation code ever so slightly

--- js/crimeScene.js
+++ js/crimeScene.js
@@ -3,15 +3,13 @@ var suspects = null;
 var witnesses = null;
 
 $(document).ready(function () {
-  $(".sceneOneScreen").toggle();
-  $(".menuArea").toggle();
+  $(".sceneOneScreen, .menuArea").toggle();
   // init the crime scene
   startCrimeScene = function(){
     suspects = false;
     witnesses = false;
     aboutVictim = false;
-    $(".playerPortrait").toggle();
-    $(".npcPortrait").toggle();
+    $(".playerPortrait, .npcPortrait, .playerSpeach, .npcSpeach").toggle();
   };
   startCrimeScene();
   // crime scene music control
@@ -36,23 +34,20 @@ $(document).ready(function () {
   getCloser = function(){
     $(".playerSpeach").html('I need to get closer');
     setTimeout(function() {
-      $(".playerSpeach").html("");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
     }, 3000);
   };
   // clear speach after a few seconds
   speakClear = function() {
     var words = $(".playerSpeach").html();
     setTimeout(function() {
-      $(".playerSpeach").html("");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
     }, words.length * 75);
   };
   npcSpeakClear = function() {
     var words = $(".npcSpeach").html();
     setTimeout(function() {
-      $(".npcSpeach").html("");
-      $(".npcPortrait").toggle();
+      $(".npcPortrait, .npcSpeach").toggle();
     }, words.length * 75);
   };
 
@@ -61,10 +56,10 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($(this).position().left) + ($(this).position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 35 && action === "Look at" || distance < -280 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 35 && action === "Look at" || distance > -280 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       $(".playerSpeach").html("Looks like the last known whereabouts of our victim... a John Doe");
       speakClear();
     }
@@ -77,10 +72,10 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($("#shards").position().left) + ($("#shards").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 60 && action === "Look at" || distance < -220 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 60 && action === "Look at" || distance > -220 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       $(".playerSpeach").html("Shards of glass... There's blood everywhere.");
       speakClear();     
     }
@@ -93,15 +88,15 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($("#head").position().left) + ($("#head").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 35 && action === "Look at" || distance < -260 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 35 && action === "Look at" && aboutVictim == false || distance > -260 && action === "Look at" && aboutVictim == false) {
       $(".playerSpeach").html("There are ways to get ahead in life, and this aint one of 'em, unless you're the murderer...Then this is Exactly how you get a head");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     } else if (distance < 35 && action === "Look at" && aboutVictim == true || distance > -260 && action === "Look at" && aboutVictim == true) {
       $(".playerSpeach").html("Twisted off... who... or what could do this?");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
@@ -114,23 +109,23 @@ $(document).ready(function () {
     var distance = (($("#car").position().left) + ($("#car").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (action === "Look at") {
       $(".playerSpeach").html("My wheels, she aint much to look at, but it beats walking");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
   $("#car").hover(function(){
     $(".helper").html("The Dick-Mobile");
   });
-
+  
   $("#brokenWindow").click(function(e) {
     var action = $(".playerAction").html();
     var distance = (($("#brokenWindow").position().left) + ($("#brokenWindow").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > -65 && action === "Look at" || distance < -400 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < -65 && action === "Look at" || distance > -400 && action === "Look at") {
       $(".playerSpeach").html("I guess this is where these shards of glass came from. What does this have to do with the murder? Could this be a robbery gone wrong?");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
@@ -143,7 +138,7 @@ $(document).ready(function () {
     var distance = (($("#handPrint").position().left) + ($("#handPrint").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (action === "Look at") {
       $(".playerSpeach").html("I should run this for prints back at HQ");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
@@ -155,11 +150,11 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($("#urine").position().left) + ($("#urine").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 60 && action === "Look at" || distance < -220 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 60 && action === "Look at" || distance > -220 && action === "Look at") {
       $(".playerSpeach").html("A puddle of urine... I should scoop some up for analysis");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear();     
     }
   });
@@ -171,15 +166,15 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($("#syringe").position().left) + ($("#syringe").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 35 && action === "Look at" || distance < -160 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 35 && action === "Look at" && suspects == false || distance > -160 && action === "Look at" && suspects == false) {
       $(".playerSpeach").html("Hmmm, Could this be related? Or maybe some crack head left it here? This place is pretty seedy...");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     } else if (distance < 35 && action === "Look at" && suspects == true || distance > -160 && action === "Look at" && suspects == true) {
       $(".playerSpeach").html("So this belongs to the crack head after all. I don't need it.");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
@@ -191,11 +186,11 @@ $(document).ready(function () {
     var action = $(".playerAction").html();
     var distance = (($("#npcRodriguez").position().left) + ($("#npcRodriguez").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 135 && action === "Look at" || distance < -460 && action === "Look at") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 135 && action === "Look at" || distance > -460 && action === "Look at") {
       $(".playerSpeach").html("Officer Rodriguez. He looks pretty shaken up. Didn't even know he smokes..");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear(); 
     }
   });
--- js/rodriguezConvo.js
+++ js/rodriguezConvo.js
@@ -1,16 +1,15 @@
 $(document).ready(function () {
-
   $(".options").toggle();
 
   $("#npcRodriguez").click(function(e) {
     var action = $(".playerAction").html();
     var distance = (($("#npcRodriguez").position().left) + ($("#npcRodriguez").position().top)) - (($("#player").position().left) + ($("#player").position().top));
     if (distance > 135 && action === "Talk to" || distance < -460 && action === "Talk to") {
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       getCloser();
     } else if (distance < 135 && action === "Talk to" || distance > -460 && action === "Talk to") {
       $(".playerSpeach").html("Officer Rodriguez. What's the deal here?");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear();
       setTimeout(RodriguezTalk, 3000);
     }
@@ -20,7 +19,7 @@ $(document).ready(function () {
   }
 
   RodriguezTalk = function(){
-    $(".npcPortrait").toggle();
+    $(".npcPortrait, .npcSpeach").toggle();
     $(".npcSpeach").html("It's not great, Dick. Somebody got messed up here real good....or bad.. I'm so confused right now..");
     npcSpeakClear();
     setTimeout(toggleOptions, 6000);
@@ -30,18 +29,18 @@ $(document).ready(function () {
     $(this).css("color", "grey");
     aboutVictim = true;
     toggleOptions();
-    $(".npcPortrait").toggle();
+    $(".npcPortrait, .npcSpeach").toggle();
     $(".npcSpeach").html("Male Caucasian of unknown identity, roughly 35-40 years of age, broken knee, decapitated and 5\"11...I think.");
     npcSpeakClear();
     response = function(){
       $(".playerSpeach").html("How was he decapitated?");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear();
     };
     setTimeout(response, 8000);
     npcResponse = function(){
       setTimeout(function(){
-        $(".npcPortrait").toggle();
+        $(".npcPortrait, .npcSpeach").toggle();
         $(".npcSpeach").html("His head was twisted completely off. it would take someone with incredible strength to do this");
       }, 4000);
       npcSpeakClear();
@@ -54,18 +53,18 @@ $(document).ready(function () {
     $(this).css("color", "grey");
     suspects = true;
     toggleOptions();
-    $(".npcPortrait").toggle();
+    $(".npcPortrait, .npcSpeach").toggle();
     $(".npcSpeach").html("We have a possible suspect or witness down in lock up now. Some Crack head, that's his needle right there.");
     npcSpeakClear();
     response = function(){
       $(".playerSpeach").html("I'll go shake him down after I look around");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear();
     };
     setTimeout(response, 8000);
     npcResponse = function(){
       setTimeout(function(){
-        $(".npcPortrait").toggle();
+        $(".npcPortrait, .npcSpeach").toggle();
         $(".npcSpeach").html("Good call. Something just feels wrong about all of this, Dick");
       }, 4000);
       npcSpeakClear();
@@ -77,18 +76,18 @@ $(document).ready(function () {
     $(this).css("color", "grey");
     witnesses = true;
     toggleOptions();
-    $(".npcPortrait").toggle();
+    $(".npcPortrait, .npcSpeach").toggle();
     $(".npcSpeach").html("None apart from the crack head we caught. I'm not sure if he even knows his own name though. Think he said it was Mahflnme");
     npcSpeakClear();
     response = function(){
       $(".playerSpeach").html("How can nobody have seen a man get his head removed?");
-      $(".playerPortrait").toggle();
+      $(".playerPortrait, .playerSpeach").toggle();
       speakClear();
     };
     setTimeout(response, 8000);
     npcResponse = function(){
       setTimeout(function(){
-        $(".npcPortrait").toggle();
+        $(".npcPortrait, .npcSpeach").toggle();
         $(".npcSpeach").html("Beats me, Dick. You'll need your head screwed on for this case. Sorry... I should head off.. heh heh... sigh.");
       }, 3500);
       npcSpeakClear();
@@ -98,14 +97,9 @@ $(document).ready(function () {
   });
   $(".bye").click(function(){
     $(this).css("color", "grey");
-    $(".npcPortrait").toggle();
+    $(".npcPortrait, .npcSpeach").toggle();
     $(".npcSpeach").html("See ya bud");
     npcSpeakClear();
     toggleOptions();
   });
-
-
-
-     
-
 });
